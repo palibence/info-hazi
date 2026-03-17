a6d2e7d9347f2a55c1255902b97dcd76617e0b44
Páli Bence <164310147+palibence@users.noreply.github.com>
2026-03-17 19:18:31 +0100

    2026.03.17

---
+++ 2026.03.17/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    // a) feladat
+    std::cout << "Egy...";
+    std::cout << "Kettő...";
+    std::cout << "Három...";
+
+    // elválasztás
+    std::cout << "\n\n";
+
+    // b) feladat
+    std::cout << "Egy...\n";
+    std::cout << "Kettő...\n";
+    std::cout << "Három...\n";
+
+    // elválasztás
+    std::cout << "\n\n";
+
+    // c) feladat
+    std::cout << "Egy...\n\n";
+    std::cout << "Kettő...\n\n";
+    std::cout << "Hátom...\n\n";
+
+    return 0;
+}
